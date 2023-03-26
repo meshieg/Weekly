32e6eaf6f5ad06614de7e6194f1ede18534f27c5
omerd318 <omer.damari@gmail.com>
2023-03-26 22:05:34 +0300

    Add(general.css): Buttons design

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,8 @@ const Home = () => {
   return (
     <div>
       <h1>Weekly</h1>
+      <button className="btn btn__primary" style={{ margin: "2rem" }}>עדי ילדה קרצייה ומעצבנת</button>
+      <button className="btn btn__secondary">התחבר</button>
     </div>
   );
 };
