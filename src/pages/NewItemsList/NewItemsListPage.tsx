f287fd8c394a9271fc75bfb0f594bec305b28e1c
Shir Konrad <shir.konrad@gmail.com>
2023-05-11 18:21:12 +0300

    Add(ScheduleItemsList): create generic list component for both my tasks and new tasks screens

--- src/pages/NewItemsList/NewItemsListPage.tsx
+++ src/pages/NewItemsList/NewItemsListPage.tsx
@@ -1,15 +1,16 @@
 import "./NewItemsListPage.css";
-import { useEffect } from 'react';
-import NewItemsList from "../../components/NewItemsList/NewItemsList";
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useNewItemsContext } from "../../contexts/NewItemsStore/NewItemsContext";
 import { ScheduleService } from "../../services/schedule.service";
 import ActionButtons from "../../assets/ActionButtons/ActionButtons";
 import useToolbar from "../../customHooks/useToolbar";
+import ScheduleItemsList from "../../components/ScheduleItemsList/ScheduleItemsList";
+import { ItemType } from "../../utils/constants";
 
-const NewItemsListPage = (
-) => {
-  const { newTasks, newEvents, removeItem, refreshItems } = useNewItemsContext();
+const NewItemsListPage = () => {
+  const { newTasks, newEvents, removeItem, refreshItems } =
+    useNewItemsContext();
   const { setToolbar } = useToolbar();
   const navigate = useNavigate();
 
@@ -22,31 +23,42 @@ const NewItemsListPage = (
       .then(() => console.log("Items saved successfully"))
       .catch((error) => console.log(error));
 
-    refreshItems(); 
+    refreshItems();
   };
 
   const onCancelClick = () => {
     // TODO: Add 'are you sure...' question
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
     <>
-      {newTasks.length === 0 ? (
-      <h3 className="new-tasks__message">
-        {`You have no new items to save :/ \n
+      {newTasks.length === 0 && newEvents.length === 0 ? (
+        <h3 className="new-tasks__message">
+          {`You have no new items to save :/ \n
         But you can always add them here :)`}
-      </h3>
+        </h3>
       ) : (
-      <>
-        <ActionButtons 
-          primaryText="Schedule!"
-          primaryAction={scheduleItems}
-          secondaryText="Cancel"
-          secondaryAction={onCancelClick}/>
-        <NewItemsList items={newTasks} removeItem={removeItem} />
-      </>)}
-      <Link to='/add-task'>
+        <>
+          <ActionButtons
+            primaryText="Schedule!"
+            primaryAction={scheduleItems}
+            secondaryText="Cancel"
+            secondaryAction={onCancelClick}
+          />
+          <ScheduleItemsList
+            items={newTasks}
+            type={ItemType.TASK}
+            onDeleteClick={removeItem}
+          />
+          <ScheduleItemsList
+            items={newEvents}
+            type={ItemType.EVENT}
+            onDeleteClick={removeItem}
+          />
+        </>
+      )}
+      <Link to="/add-task">
         <button>Add</button>
       </Link>
     </>
