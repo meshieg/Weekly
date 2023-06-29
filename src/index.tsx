edc288e03b225d5ddb607e78133dfa989fca9cf7
gmesh <gmeshie@gmail.com>
2023-06-29 18:39:10 +0300

    Add(Google sign in/up): return the feature

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  // <GoogleOAuthProvider clientId="601541058375-urmmdgs0qukjjfv7716g8e8sk6p1qtat.apps.googleusercontent.com">
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  // </GoogleOAuthProvider>
+  <GoogleOAuthProvider clientId="601541058375-urmmdgs0qukjjfv7716g8e8sk6p1qtat.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
